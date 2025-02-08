b9fc7c172e8fc4f92cffae17639cc3c2001720df
bastian.bumann <bastian.bumann@borm.ch>
2025-02-08 21:25:06 +0100

    Borm Cleanup

--- flask/custom_rest/projects_talking/endpoints.py
+++ flask/custom_rest/projects_talking/endpoints.py
@@ -2,7 +2,7 @@
 # Import des request Moduls um die Anfrage zu bearbeiten.
 from flask import Blueprint, request 
 # Import des Kundenspezifischen Kontaktmoduls um die Projektinfos abzufragen.
-from custom.projects_talking.projects_talking import get_pro_infos
+from custom.projects_talking.projects_talking import get_new_duedate
 
 # Endpunkt "adresse" registrieren
 api = Blueprint('projects_talking', __name__)
@@ -10,13 +10,13 @@ api = Blueprint('projects_talking', __name__)
 
 # Endpunkt /cadwork registrieren um Projektinfos anhand Dateipfad
 # und Dateinamen abzufragen.
-@api.route('/projekt')
-def get_project():
+@api.route('/duedate')
+def set_duedate():
     """Dateiinfos aus der URL holen"""
     # Abfrage der Anfragedaten
-    filepath = request.args.get("filepath")
-    filename = request.args.get("filename")
+    guid = request.args.get("guid")
+    date = request.args.get("date")
     # Aufruf der get_contact Funktion aus dem Kundenspezifischen Adressmoduls.
-    data = get_pro_infos(filepath, filename)
+    data = get_new_duedate(guid, date)
     # Senden der Antwort
     return {"project": data}
